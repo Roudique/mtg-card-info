64913a1df7de8c592f3173c018b566550c086009
Oleksandr Rudavka <roudique@gmail.com>
2018-02-15 11:07:40 -0500

    Initial Commit

---
+++ MTG Card Price/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MTG Card Price
+//
+//  Created by Oleksandr Rudavka on 2018-02-15.
+//  Copyright © 2018 roudique. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MTG Card Price/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MTG Card Price
+//
+//  Created by Oleksandr Rudavka on 2018-02-15.
+//  Copyright © 2018 roudique. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
